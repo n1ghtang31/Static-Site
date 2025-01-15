ee1bc444956ce1f9e1371c6c8112cb7c7826f1a5
Craig Rossiter <crossiter0@gmail.com>
2025-01-15 10:43:39 +0000

    Lesson 13 completed added ability to move static items to public folder

--- src/main.py
+++ src/main.py
@@ -1,8 +1,50 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+LOGGING = 1
+source = "./static"
+destination = "./public"
 
 def main():
-    text = TextNode("This is my test", TextType.NORMAL, "http://www.google.com")
+    transfer_static_to_public(source, destination)
 
-    print(text)
+def extra_log(text, variable_key):
+    if LOGGING == 0:
+        return
+    elif LOGGING == 1:
+        return print(f"{text}: {variable_key}")
+    else:
+        raise ValueError("LOGGING Value is set incorrectly")
+
+
+def transfer_static_to_public(source, destination):
+    if not os.path.exists(source):
+        raise FileNotFoundError(f"Source folder is missing: {source}")
+    if not os.path.exists(destination):
+        extra_log("folder does not exist creating", destination)
+        os.makedirs(destination)
+    else:
+        # Remove all files in the original destination folder
+        for item in os.listdir(destination):
+            item_path = os.path.join(destination, item)
+            if os.path.isdir(item_path):
+                extra_log("removing folder", item_path)
+                shutil.rmtree(item_path)
+            else:
+                extra_log("removing", item_path)
+                os.remove(item_path)
+
+    for item in os.listdir(source):
+        source_path = os.path.join(source, item)
+        destination_path = os.path.join(destination, item)
+        if item[0] == ".":
+            continue
+
+        if os.path.isdir(source_path):
+            transfer_static_to_public(source_path, destination_path)
+        else:
+            extra_log("copying file", item)
+            shutil.copy(source_path, destination_path)
+
 
 main()
